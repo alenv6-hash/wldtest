5f6f5be5fb7cc6b402351117a1387288467ba5ee
alenv6-hash <alenv6@gmail.com>
2025-11-07 15:16:28 +0100

    Add files via upload

---
+++ tilesets/wld_congress_mac.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="wld_congress_mac copy" tilewidth="224" tileheight="64" tilecount="1" columns="1">
+ <image source="wld_congress_mac copy.png" width="224" height="64"/>
+</tileset>
